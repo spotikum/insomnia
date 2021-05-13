adc775553c8f114be90647c00191a9649d516b14
bukanspot <spot@tracert.id>
2021-05-14 00:43:40 +0800

    update tampilan regis user

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -13,7 +13,7 @@
                         <div class="col-lg-6">
                             <div class="p-5">
                                 <div class="text-center">
-                                    <h1 class="h4 text-gray-900 mb-4">Hi Njing</h1>
+                                    <h1 class="h4 text-gray-900 mb-4">Hi User</h1>
                                 </div>
                                 <form class="user" method="POST" action="{{ route('login') }}">
                                     @csrf
