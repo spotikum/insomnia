4b8e55f717b0a08af443d08f646c5d846f098110
bukanspot <spot@tracert.id>
2021-05-20 03:23:21 +0800

    edit detail

--- resources/views/user/shop/detail.blade.php
+++ resources/views/user/shop/detail.blade.php
@@ -101,149 +101,18 @@
                               <a class="nav-link active" data-toggle="tab" href="#tabs-1" role="tab">Description</a>
                          </li>
                          <li class="nav-item">
-                              <a class="nav-link" data-toggle="tab" href="#tabs-2" role="tab">Specification</a>
-                         </li>
-                         <li class="nav-item">
                               <a class="nav-link" data-toggle="tab" href="#tabs-3" role="tab">Reviews ( 2 )</a>
                          </li>
                          </ul>
                          <div class="tab-content">
-                         <div class="tab-pane active" id="tabs-1" role="tabpanel">
-                              <h6>Description</h6>
-                              <p>Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut loret fugit, sed
-                                   quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt loret.
-                                   Neque porro lorem quisquam est, qui dolorem ipsum quia dolor si. Nemo enim ipsam
-                                   voluptatem quia voluptas sit aspernatur aut odit aut loret fugit, sed quia ipsu
-                                   consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Nulla
-                              consequat massa quis enim.</p>
-                              <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget
-                                   dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes,
-                                   nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium
-                              quis, sem.</p>
-                         </div>
-                         <div class="tab-pane" id="tabs-2" role="tabpanel">
-                              <h6>Specification</h6>
-                              <p>Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut loret fugit, sed
-                                   quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt loret.
-                                   Neque porro lorem quisquam est, qui dolorem ipsum quia dolor si. Nemo enim ipsam
-                                   voluptatem quia voluptas sit aspernatur aut odit aut loret fugit, sed quia ipsu
-                                   consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Nulla
-                              consequat massa quis enim.</p>
-                              <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget
-                                   dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes,
-                                   nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium
-                              quis, sem.</p>
-                         </div>
-                         <div class="tab-pane" id="tabs-3" role="tabpanel">
-                              <h6>Reviews ( 2 )</h6>
-                              <p>Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut loret fugit, sed
-                                   quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt loret.
-                                   Neque porro lorem quisquam est, qui dolorem ipsum quia dolor si. Nemo enim ipsam
-                                   voluptatem quia voluptas sit aspernatur aut odit aut loret fugit, sed quia ipsu
-                                   consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Nulla
-                              consequat massa quis enim.</p>
-                              <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget
-                                   dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes,
-                                   nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium
-                              quis, sem.</p>
-                         </div>
-                         </div>
-                    </div>
-               </div>
-          </div>
-          <div class="row">
-               <div class="col-lg-12 text-center">
-                    <div class="related__title">
-                         <h5>RELATED PRODUCTS</h5>
-                    </div>
-               </div>
-               <div class="col-lg-3 col-md-4 col-sm-6">
-                    <div class="product__item">
-                         <div class="product__item__pic set-bg" data-setbg="assets/user/img/product/related/rp-1.jpg">
-                         <div class="label new">New</div>
-                         <ul class="product__hover">
-                              <li><a href="assets/user/img/product/related/rp-1.jpg" class="image-popup"><span class="arrow_expand"></span></a></li>
-                              <li><a href="#"><span class="icon_heart_alt"></span></a></li>
-                              <li><a href="#"><span class="icon_bag_alt"></span></a></li>
-                         </ul>
-                         </div>
-                         <div class="product__item__text">
-                         <h6><a href="#">Buttons tweed blazer</a></h6>
-                         <div class="rating">
-                              <i class="fa fa-star"></i>
-                              <i class="fa fa-star"></i>
-                              <i class="fa fa-star"></i>
-                              <i class="fa fa-star"></i>
-                              <i class="fa fa-star"></i>
-                         </div>
-                         <div class="product__price">$ 59.0</div>
-                         </div>
-                    </div>
-               </div>
-               <div class="col-lg-3 col-md-4 col-sm-6">
-                    <div class="product__item">
-                         <div class="product__item__pic set-bg" data-setbg="assets/user/img/product/related/rp-2.jpg">
-                         <ul class="product__hover">
-                              <li><a href="assets/user/img/product/related/rp-2.jpg" class="image-popup"><span class="arrow_expand"></span></a></li>
-                              <li><a href="#"><span class="icon_heart_alt"></span></a></li>
-                              <li><a href="#"><span class="icon_bag_alt"></span></a></li>
-                         </ul>
-                         </div>
-                         <div class="product__item__text">
-                         <h6><a href="#">Flowy striped skirt</a></h6>
-                         <div class="rating">
-                              <i class="fa fa-star"></i>
-                              <i class="fa fa-star"></i>
-                              <i class="fa fa-star"></i>
-                              <i class="fa fa-star"></i>
-                              <i class="fa fa-star"></i>
-                         </div>
-                         <div class="product__price">$ 49.0</div>
-                         </div>
-                    </div>
-               </div>
-               <div class="col-lg-3 col-md-4 col-sm-6">
-                    <div class="product__item">
-                         <div class="product__item__pic set-bg" data-setbg="assets/user/img/product/related/rp-3.jpg">
-                         <div class="label stockout">out of stock</div>
-                         <ul class="product__hover">
-                              <li><a href="assets/user/img/product/related/rp-3.jpg" class="image-popup"><span class="arrow_expand"></span></a></li>
-                              <li><a href="#"><span class="icon_heart_alt"></span></a></li>
-                              <li><a href="#"><span class="icon_bag_alt"></span></a></li>
-                         </ul>
-                         </div>
-                         <div class="product__item__text">
-                         <h6><a href="#">Cotton T-Shirt</a></h6>
-                         <div class="rating">
-                              <i class="fa fa-star"></i>
-                              <i class="fa fa-star"></i>
-                              <i class="fa fa-star"></i>
-                              <i class="fa fa-star"></i>
-                              <i class="fa fa-star"></i>
-                         </div>
-                         <div class="product__price">$ 59.0</div>
-                         </div>
-                    </div>
-               </div>
-               <div class="col-lg-3 col-md-4 col-sm-6">
-                    <div class="product__item">
-                         <div class="product__item__pic set-bg" data-setbg="assets/user/img/product/related/rp-4.jpg">
-                         <ul class="product__hover">
-                              <li><a href="assets/user/img/product/related/rp-4.jpg" class="image-popup"><span class="arrow_expand"></span></a></li>
-                              <li><a href="#"><span class="icon_heart_alt"></span></a></li>
-                              <li><a href="#"><span class="icon_bag_alt"></span></a></li>
-                         </ul>
-                         </div>
-                         <div class="product__item__text">
-                         <h6><a href="#">Slim striped pocket shirt</a></h6>
-                         <div class="rating">
-                              <i class="fa fa-star"></i>
-                              <i class="fa fa-star"></i>
-                              <i class="fa fa-star"></i>
-                              <i class="fa fa-star"></i>
-                              <i class="fa fa-star"></i>
-                         </div>
-                         <div class="product__price">$ 59.0</div>
+                              <div class="tab-pane active" id="tabs-1" role="tabpanel">
+                                   <h6>Description</h6>
+                                   {{ $product->description }}
+                              </div>
+                              <div class="tab-pane" id="tabs-3" role="tabpanel">
+                                   <h6>Reviews ( 2 )</h6>
+                                   <p>nothing</p>
+                              </div>
                          </div>
                     </div>
                </div>
