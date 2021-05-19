e1b9724968048a6585fc0ee452ea53810521e069
bukanspot <spot@tracert.id>
2021-05-20 04:09:39 +0800

    remove shop by price

--- resources/views/user/shop/shop.blade.php
+++ resources/views/user/shop/shop.blade.php
@@ -31,23 +31,6 @@
                             </div>
                         </div>
                     </div>
-                    <div class="sidebar__filter">
-                        <div class="section-title">
-                            <h4>Shop by price</h4>
-                        </div>
-                        <div class="filter-range-wrap">
-                            <div class="price-range ui-slider ui-corner-all ui-slider-horizontal ui-widget ui-widget-content"
-                            data-min="33" data-max="99"></div>
-                            <div class="range-slider">
-                                <div class="price-input">
-                                    <p>Price:</p>
-                                    <input type="text" id="minamount">
-                                    <input type="text" id="maxamount">
-                                </div>
-                            </div>
-                        </div>
-                        <a href="#">Filter</a>
-                    </div>
                     <div class="sidebar__sizes">
                         <div class="section-title">
                             <h4>Shop by rating</h4>
