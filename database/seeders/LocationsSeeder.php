90027534377cb3191ecc4badb8ad8ad2198b50f3
bukanspot <spot@tracert.id>
2021-06-01 06:16:16 +0800

    make Seeder

--- database/seeders/LocationsSeeder.php
+++ database/seeders/LocationsSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Kavist\RajaOngkir\Facades\RajaOngkir;
+use App\Models\Province;
+use App\Models\City;
 
 class LocationsSeeder extends Seeder
 {
@@ -13,6 +16,20 @@ class LocationsSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $daftarProvinsi = RajaOngkir::provinsi()->all();
+        foreach ($daftarProvinsi as $provinceRow) {
+            Province::create([
+                'province_id' => $provinceRow['province_id'],
+                'name'        => $provinceRow['province'],
+            ]);
+            $daftarKota = RajaOngkir::kota()->dariProvinsi($provinceRow['province_id'])->get();
+            foreach ($daftarKota as $cityRow) {
+                City::create([
+                    'province_id'   => $provinceRow['province_id'],
+                    'city_id'       => $cityRow['city_id'],
+                    'name'          => $cityRow['city_name'],
+                ]);
+            }
+        }
     }
 }
